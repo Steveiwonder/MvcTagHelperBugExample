d6805cc7e564aea624ebf1fab7462e347a5c8645
Steven Yates <s_yates1@hotmail.com>
2018-11-09 12:39:37 +0000

    init

---
+++ WebApplication11/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication11.Controllers
+{
+    public class HomeController: Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(new IndexViewModel()
+            {
+                People = new List<Person>()
+                {
+                    new Person()
+                    {
+                        Name = "Bob",
+                        Id= 1
+                    }
+                }
+            });
+        }
+
+        
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+    public class IndexViewModel
+    {
+        public bool ACheckbox { get; set; }
+        public List<Person> People { get; set; }
+        public int SelectedPerson { get; set; }
+    }
+}
---
+++ WebApplication11/TagHelpers/InputTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApplication11.TagHelpers
+{
+    [HtmlTargetElement("input", Attributes = "asp-for")]
+    public class MyInputTagHelper : InputTagHelper
+    {
+        public MyInputTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+    }
+    [HtmlTargetElement("select", Attributes = "asp-for")]
+    public class MySelectTagHelper : SelectTagHelper
+    {
+        public MySelectTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+    }
+}
